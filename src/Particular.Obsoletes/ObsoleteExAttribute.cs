eff714b487d5ee182479d811a6c8b67b18b5af5d
Brandon Ording <bording@gmail.com>
2025-06-30 19:49:24 -0400

    Add ObsoleteExAttribute

---
+++ src/Particular.Obsoletes/ObsoleteExAttribute.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+
+namespace Particular.Obsoletes;
+
+using System.Diagnostics;
+
+[Conditional("PARTICULAR_OBSOLETES")]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false)]
+sealed class ObsoleteExAttribute : Attribute
+{
+    public string? Message { get; set; }
+
+    public string? TreatAsErrorFromVersion { get; set; }
+
+    public string? RemoveInVersion { get; set; }
+
+    public string? ReplacementTypeOrMember { get; set; }
+}
