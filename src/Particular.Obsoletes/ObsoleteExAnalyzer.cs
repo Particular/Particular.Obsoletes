d3303ec409711ebb02a6232476bf54206fd8f18c
Brandon Ording <bording@gmail.com>
2025-06-30 19:21:22 -0400

    Add starting code

---
+++ src/Particular.Obsoletes/ObsoleteExAnalyzer.cs
@@ -0,0 +1,88 @@
+﻿namespace Particular.Obsoletes;
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class ObsoleteExAnalyzer : DiagnosticAnalyzer
+{
+    static readonly ImmutableArray<SyntaxKind> syntaxKinds = [SyntaxKind.ClassDeclaration, SyntaxKind.StructDeclaration, SyntaxKind.InterfaceDeclaration, SyntaxKind.EnumDeclaration, SyntaxKind.ConstructorDeclaration, SyntaxKind.MethodDeclaration, SyntaxKind.PropertyDeclaration, SyntaxKind.FieldDeclaration, SyntaxKind.EventDeclaration, SyntaxKind.DelegateDeclaration];
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [];
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(AnalyzeSyntax, syntaxKinds);
+    }
+
+    static void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+    {
+        if (context.Node is MemberDeclarationSyntax member && member.AttributeLists.Any())
+        {
+            AttributeSyntax? obsoleteEx = null;
+            AttributeSyntax? obsolete = null;
+
+            foreach (var attributeListSyntax in member.AttributeLists)
+            {
+                foreach (var attributeSyntax in attributeListSyntax.Attributes)
+                {
+                    SyntaxToken? nameToken = attributeSyntax.Name switch
+                    {
+                        IdentifierNameSyntax name => name.Identifier,
+                        QualifiedNameSyntax name => name.Right.Identifier,
+                        _ => null
+                    };
+
+                    if (nameToken is null)
+                    {
+                        continue;
+                    }
+
+                    if (nameToken.Value.ValueText is "ObsoleteEx")
+                    {
+                        obsoleteEx = attributeSyntax;
+                    }
+                    else if (nameToken.Value.ValueText is "Obsolete")
+                    {
+                        obsolete = attributeSyntax;
+                    }
+                }
+            }
+
+            if (obsoleteEx is not null || obsolete is not null)
+            {
+                Analyze(context, obsoleteEx, obsolete);
+            }
+        }
+    }
+
+    static void Analyze(SyntaxNodeAnalysisContext context, AttributeSyntax? obsoleteEx, AttributeSyntax? obsolete)
+    {
+        _ = context;
+        _ = obsoleteEx;
+        _ = obsolete;
+
+        //if (context.SemanticModel.GetSymbolInfo(attributeSyntax).Symbol is not IMethodSymbol attributeSymbol)
+        //{
+        //    continue;
+        //}
+
+        //var attributeContainingTypeSymbol = attributeSymbol.ContainingType;
+        //var fullName = attributeContainingTypeSymbol.ToDisplayString();
+
+        //if (fullName == "Particular.Obsoletes.ObsoleteExAttribute")
+        //{
+        //    obsoleteEx = attributeSymbol;
+        //}
+        //else if (fullName == "System.ObsoleteAttribute")
+        //{
+        //    obsolete = attributeSymbol;
+        //}
+    }
+
+}
---
+++ src/Tests/ObsoleteExAnalyzerTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Particular.Analyzers.Tests
+{
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Particular.Analyzers.Tests.Helpers;
+    using Particular.Obsoletes;
+
+    public class ObsoleteExAnalyzerTests : AnalyzerTestFixture<ObsoleteExAnalyzer>
+    {
+        [Test]
+        public Task Test()
+        {
+            var code = """
+
+                using System.Diagnostics;
+                using Particular.Obsoletes;
+
+                namespace Particular.Obsoletes
+                {
+                [Conditional("PARTICULAR_OBSOLETES")]
+                [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false)]
+                sealed class ObsoleteExAttribute : Attribute
+                {
+                    public string Message { get; set; }
+
+                    public string TreatAsErrorFromVersion { get; set; }
+
+                    public string RemoveInVersion { get; set; }
+
+                   public string ReplacementTypeOrMember { get; set; }
+                }
+                }
+
+
+
+                [Obsolete]
+                [global::Particular.Obsoletes.ObsoleteEx(Message = "blah")]
+                public class Foo
+                {
+
+                }
+                """;
+
+            return Assert(code);
+        }
+    }
+}
