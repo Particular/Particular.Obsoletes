48b95e59449c6d8a23c5b7d396334922fce2a5ed
Brandon Ording <bording@gmail.com>
2025-06-30 22:18:14 -0400

    Add Fixes project

---
+++ src/Particular.Obsoletes.Fixes/ObsoleteExCodeFixProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Particular.Obsoletes.Fixes;
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+class ObsoleteExCodeFixProvider : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds => throw new NotImplementedException();
+
+    public override Task RegisterCodeFixesAsync(CodeFixContext context) => throw new NotImplementedException();
+}
