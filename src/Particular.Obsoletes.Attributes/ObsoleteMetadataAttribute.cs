520cc909e992b80c7c0f83d97ca32e0efea01373
Brandon Ording <bording@gmail.com>
2025-07-03 11:21:29 -0400

    Move to using separate Attributes assembly instead

--- src/Particular.Obsoletes.Attributes/ObsoleteMetadataAttribute.cs
+++ src/Particular.Obsoletes.Attributes/ObsoleteMetadataAttribute.cs
@@ -5,15 +5,30 @@ namespace Particular.Obsoletes;
 using System;
 using System.Diagnostics;
 
+/// <summary>
+///
+/// </summary>
 [Conditional("PARTICULAR_OBSOLETES")]
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false)]
-sealed class ObsoleteMetadataAttribute : Attribute
+public sealed class ObsoleteMetadataAttribute : Attribute
 {
+    /// <summary>
+    ///
+    /// </summary>
     public string? Message { get; set; }
 
+    /// <summary>
+    ///
+    /// </summary>
+    public string? ReplacementTypeOrMember { get; set; }
+
+    /// <summary>
+    ///
+    /// </summary>
     public string? TreatAsErrorFromVersion { get; set; }
 
+    /// <summary>
+    ///
+    /// </summary>
     public string? RemoveInVersion { get; set; }
-
-    public string? ReplacementTypeOrMember { get; set; }
 }
