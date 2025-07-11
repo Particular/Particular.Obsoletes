4c349e8473022cf4a95397aaa7b6499e4b7ecce4
Brandon Ording <bording@gmail.com>
2025-07-11 12:11:28 -0400

    Add polyfill to simplify record

--- src/Particular.Obsoletes/ObsoleteMetadataAttributeValues.cs
+++ src/Particular.Obsoletes/ObsoleteMetadataAttributeValues.cs
@@ -1,24 +1,3 @@
 ﻿namespace Particular.Obsoletes;
 
-readonly record struct ObsoleteMetadataAttributeValues
-{
-    public ObsoleteMetadataAttributeValues(string? message, string? treatAsErrorFromVersion, bool treatAsErrorFromVersionSet, string? removeInVersion, bool removeInVersionSet, string? replacementTypeOrMember)
-    {
-        Message = message;
-
-        TreatAsErrorFromVersion = treatAsErrorFromVersion;
-        TreatAsErrorFromVersionSet = treatAsErrorFromVersionSet;
-
-        RemoveInVersion = removeInVersion;
-        RemoveInVersionSet = removeInVersionSet;
-
-        ReplacementTypeOrMember = replacementTypeOrMember;
-    }
-
-    public readonly string? Message;
-    public readonly string? TreatAsErrorFromVersion;
-    public readonly bool TreatAsErrorFromVersionSet;
-    public readonly string? RemoveInVersion;
-    public readonly bool RemoveInVersionSet;
-    public readonly string? ReplacementTypeOrMember;
-}
+readonly record struct ObsoleteMetadataAttributeValues(string? Message, string? TreatAsErrorFromVersion, bool TreatAsErrorFromVersionSet, string? RemoveInVersion, bool RemoveInVersionSet, string? ReplacementTypeOrMember);
