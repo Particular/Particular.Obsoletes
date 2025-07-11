63501a38089ffe9b33bed358b68d75301af2b1a2
Brandon Ording <bording@gmail.com>
2025-07-11 18:37:43 -0400

    Add XML comments

--- src/Particular.Obsoletes.Attributes/ObsoleteMetadataAttribute.cs
+++ src/Particular.Obsoletes.Attributes/ObsoleteMetadataAttribute.cs
@@ -4,29 +4,29 @@ using System;
 using System.Diagnostics;
 
 /// <summary>
-///
+/// Data that the Particular.Obsoletes analyzer uses to ensure the corresponding <see cref="ObsoleteAttribute" /> is properly constructed.
 /// </summary>
 [Conditional("PARTICULAR_OBSOLETES")]
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false)]
 public sealed class ObsoleteMetadataAttribute : Attribute
 {
     /// <summary>
-    ///
+    /// The text string that describes alternative workarounds.
     /// </summary>
     public string? Message { get; set; }
 
     /// <summary>
-    ///
+    ///  A value pointing to the name of the replacement member if available.
     /// </summary>
     public string? ReplacementTypeOrMember { get; set; }
 
     /// <summary>
-    ///
+    /// The version when the <see cref="ObsoleteAttribute" /> on the member will change from a warning to an error. Must be convertible to a <see cref="Version"/>.
     /// </summary>
     public string? TreatAsErrorFromVersion { get; set; }
 
     /// <summary>
-    ///
+    /// The version when the obsolete member will be removed. Must be convertible to a <see cref="Version"/>.
     /// </summary>
     public string? RemoveInVersion { get; set; }
 }
--- src/Particular.Obsoletes.Attributes/PreObsoleteAttribute.cs
+++ src/Particular.Obsoletes.Attributes/PreObsoleteAttribute.cs
@@ -4,30 +4,30 @@ using System;
 using System.Diagnostics;
 
 /// <summary>
-///
+/// Meant for staging future obsoletes.
 /// </summary>
-/// <param name="contextUrl"></param>
+/// <param name="contextUrl">A link to a GitHub issue that provides context for why the future obsoletion is required.</param>
 [Conditional("PARTICULAR_OBSOLETES")]
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false)]
 public sealed class PreObsoleteAttribute(string contextUrl) : Attribute
 {
     /// <summary>
-    ///
+    ///  A link to a GitHub issue that provides context for why the future obsoletion is required.
     /// </summary>
     public string ContextUrl { get; } = contextUrl;
 
     /// <summary>
-    ///
+    /// A summary of the context provided in the issue that <see cref="ContextUrl" /> points to.
     /// </summary>
     public string? Note { get; set; }
 
     /// <summary>
-    ///
+    /// The text string that describes alternative workarounds.
     /// </summary>
     public string? Message { get; set; }
 
     /// <summary>
-    ///
+    /// A value pointing to the name of the replacement member if available.
     /// </summary>
     public string? ReplacementTypeOrMember { get; set; }
 }
