ad31e867aa735f6754bf47ef9935998b7ef59afc
Daniel Marbach <daniel.marbach@openplace.net>
2026-08-05 19:25:40 +0200

    Thank you David ;)

--- src/Tests/ObsoleteCodeFixProviderTests.cs
+++ src/Tests/ObsoleteCodeFixProviderTests.cs
@@ -425,7 +425,7 @@ public class ObsoleteCodeFixProviderTests : CodeFixTestFixture<ObsoleteAnalyzer,
         using Particular.Obsoletes;
 
         [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-        [ObsoleteMetadata(TreatAsErrorFromVersion = "2", RemoveInVersion = "3", DiagnosticId = "NSB0001", UrlFormat = "https://docs.particular.net/obsoletions/{0}")]
+        [ObsoleteMetadata(TreatAsErrorFromVersion = "2", RemoveInVersion = "3", DiagnosticId = "NSB0001", UrlFormat = "https://docs.particular.net/r/obsoletions/{0}")]
         [Obsolete("Will be treated as an error from version 2.0.0. Will be removed in version 3.0.0.", false, DiagnosticId = "NSB0001", UrlFormat = "https://wrong.com/{0}")]
         public class Foo
         {
